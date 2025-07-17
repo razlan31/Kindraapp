1fc63900dc2c98ca90350a50d923affe8d8fa5c8
RAZLAN SALIM <razlansalim@MUHAMMADs-Laptop.local>
2025-07-17 18:29:20 +0800

    chore: remove legacy web/client folder so Snack boots correct App.js

--- client/src/pages/profile-test.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProfileTest() {
-  return (
-    <div className="min-h-screen bg-white p-4">
-      <h1 className="text-3xl font-bold text-black">PROFILE PAGE WORKS!</h1>
-      <p className="text-lg mt-4">This is the profile test page.</p>
-    </div>
-  );
-}
